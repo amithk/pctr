02138faebf19677baa0e89e8b0593152305ec767
Amit Kulkarni <amithkamithk@gmail.com>
2018-07-26 15:56:11 +0530

    Implement basic counter operations except 'delete', untested.

--- src/pctr/counter.go
+++ src/pctr/counter.go
@@ -1,34 +1,66 @@
 package pctr
 
-type CounterValue struct {
-	val uint64
-}
+import "os"
+import "sync"
 
+// Persistent counter optimized for single process, multithreaded scenarios.
+// In other words, all accesses to the object of PersistentCounter are thread safe.
+// But if the same counter file is accessed by creating multiple objects of this
+// structure, the results can be inconsistent.
 type PersistentCounter struct {
 	counterid string
 	spath     string
-	Value     CounterValue
+	Value     uint64
 	deleted   bool
-	fileops   *FileOps
+	f         *os.File
+	mutx      *sync.Mutex
 }
 
 func NewPersistentCounter(counterid, spath string) (*PersistentCounter, error) {
 	pc := &PersistentCounter{
 		counterid: counterid,
 		spath:     spath,
+		mutx:      &sync.Mutex{},
 	}
-	pc.fileops = &FileOps{}
-	pc.fileops.CreateNew(spath)
+	f, err := CreateNew(spath)
+	if err != nil {
+		return nil, err
+	}
+	pc.f = f
+
+	val, err1 := pc.GetValue()
+	if err1 != nil {
+		return nil, err1
+	}
+
+	pc.Value = val
+	return pc, nil
 }
 
+// IncrementValue returns the updated value of the counter. This make GetValue
+// API unnecessary. For all practical purposes, NewPersistentCounter,
+// IncrementValue and DeleteCounter APIs should be enough.
 func (pc *PersistentCounter) IncrementValue(incr uint64) (uint64, error) {
+	pc.mutx.Lock()
+	newVal := pc.Value + incr
+	err := WriteFile(pc.f, newVal)
+	pc.Value = newVal
+	pc.mutx.Unlock()
+	return newVal, err
 }
 
 func (pc *PersistentCounter) DeleteCounter() error {
+	return nil
 }
 
 func (pc *PersistentCounter) GetValue() (uint64, error) {
+	return ReadFile(pc.f)
 }
 
 func (pc *PersistentCounter) IsDeleted() bool {
+	return false
+}
+
+// Persistent counter optimized for mutliprocess scenarios.
+type PersistentCounterMultiproc struct {
 }
--- src/pctr/fileops.go
+++ src/pctr/fileops.go
@@ -1,19 +1,68 @@
 package pctr
 
-type FileOps struct {
+import "encoding/binary"
+import "io"
+import "os"
+
+func CreateNew(fpath string) (*os.File, error) {
+	buf := serializeUint64(0)
+	f, err := os.OpenFile("notes.txt", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+	_, err1 := f.Write(buf)
+	if err1 != nil {
+		return nil, err
+	}
+	return f, nil
 }
 
-func (fo *FileOps) CreateNew(fpath string) error {
+// File must be closed before MoveFile is called on it.
+func MoveFile(oldPath, newPath string) error {
+	return nil
 }
 
-func (fo *FileOps) Move(oldPath, newPath string) error {
+func ReadFile(f *os.File) (uint64, error) {
+	buf := make([]byte, binary.MaxVarintLen64)
+	_, err := f.Read(buf)
+	if err == io.EOF {
+		return 0, nil
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	return deserializeUint64(buf), nil
 }
 
-func (fo *FileOps) Read(fpath string) (uint64, error) {
+func WriteFile(f *os.File, val uint64) error {
+	buf := serializeUint64(val)
+	_, err := f.WriteAt(buf, 0)
+	return err
 }
 
-func (fo *FileOps) Write(fpath string, val uint64) error {
+// File must be closed before DeleteFile is called on it.
+func DeleteFile(fpath string) error {
+	return nil
 }
 
-func (fo *FileOps) Delete(fpath string) error {
+func CloseFile(f *os.File) error {
+	return nil
+}
+
+// TODO: Not sure if OpenFile is needed
+func OpenFile(fpath string) (*os.File, error) {
+	return nil, nil
+}
+
+func serializeUint64(val uint64) []byte {
+	buf := make([]byte, binary.MaxVarintLen64)
+	binary.PutUvarint(buf, 0)
+	return buf
+}
+
+func deserializeUint64(buf []byte) uint64 {
+	val, _ := binary.Uvarint(buf)
+	return val
 }
